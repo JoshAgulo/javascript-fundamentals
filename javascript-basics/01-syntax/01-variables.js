b178cb9ce9250c668c58bc346eb93da9c460002d
Josh Agulo <agulo.joshclarence@gmail.com>
2026-02-02 12:48:08 +0800

    Add files via upload

---
+++ javascript-basics/01-syntax/01-variables.js
@@ -0,0 +1,13 @@
+// Lesson 1: Understanding, variables, Data Types, let vs const and Template Literals.
+let age = 19;
+let gpa = 3.4;
+let isEnrolled = true;
+let firstName = "John";
+let lastName = 'Doe';
+
+
+// Using backticks for cleaner string concatenation
+console.log(`Name: ${firstName} ${lastName}`);
+console.log(`Age: ${age}`);
+console.log(`Enrolled: ${isEnrolled}`);
+console.log(`GPA: ${gpa}`);
