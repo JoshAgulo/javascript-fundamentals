e6d3958535c46482634efc9be4205a49a5620a9b
Josh Agulo <agulo.joshclarence@gmail.com>
2026-02-07 00:43:59 +0800

    Add files via upload

---
+++ javascript-basics/01-syntax/04-type-conversion.js
@@ -0,0 +1,10 @@
+// Lesson 4: Manual type conversion to ensure data integrity during arithmetic or logical operations.
+let x = Number("2026.2025");
+let y = String(2027);
+let z = Boolean("text");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+
