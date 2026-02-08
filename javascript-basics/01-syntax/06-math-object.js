6fb23366a290e3bd6803586a7039c165d87e67f4
Josh Agulo <agulo.joshclarence@gmail.com>
2026-02-08 23:59:49 +0800

    Add files via upload

---
+++ javascript-basics/01-syntax/06-math-object.js
@@ -0,0 +1,12 @@
+// Lesson 6: Leveraging the built-in Math object’s static methods and constants for complex arithmetic and trigonometric logic.
+let n1 = Math.log(Math.E);
+let n2 = Math.abs(-2);
+let n3 = Math.sign(100) * 3;
+let n4 = Math.min(4, 10, 20);
+let n5 = Math.max(1, 5, 3);
+let n6 = Math.round(Math.sin(Math.PI / 2) * 6);
+let n7 = Math.round(Math.cos(0) * 7);
+let n8 = Math.round(Math.tan(Math.PI / 4) * 8);
+let n9 = Math.sqrt(81);
+
+console.log(`${n1}, ${n2}, ${n3}, ${n4}, ${n5}, ${n6}, ${n7}, ${n8}, ${n9}`);
